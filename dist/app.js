16f1db2d659759aa7f7765136fb48fb205ce3284
梁胡刚 <lianghugang@dxy.cn>
2018-11-27 19:48:11 +0800

    init

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const ReactDom = require("react-dom");
+ReactDom.render(<div>
+    hello world
+  </div>, document.getElementById('app'));
+//# sourceMappingURL=app.js.map
