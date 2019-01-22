175ab20c7f7d4ab7038956b124412d56d2ce5268
Hoo Gang <ppbaozi@gmail.com>
2019-01-23 03:55:07 +0800

    clean

--- config/webpack.prod.ts
+++
@@ -1,8 +0,0 @@
-import webpackMerge from 'webpack-merge'
-import webpackCommon from './webpack.common'
-
-const prodConf = webpackMerge(webpackCommon, {
-  mode: 'production',
-})
-
-export default prodConf
